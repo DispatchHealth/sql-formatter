2f06a731a82f5874ffeafb27009a06f17bfbcbf2
Cesar Landeros Delgado <815951+CesarLanderos@users.noreply.github.com>
2023-07-06 17:00:23 -0600

    Update webpack.common.js

--- webpack.common.js
+++ webpack.common.js
@@ -7,7 +7,7 @@ module.exports = {
     filename: 'sql-formatter.js',
     library: 'sqlFormatter',
     libraryTarget: 'umd',
-    hashFunction: 'md5',
+    hashFunction: 'xxhash64',
   },
   module: {
     rules: [
